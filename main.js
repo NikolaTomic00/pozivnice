423360732e263f28f9e00d5938d951cf276e7cc1
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-04-18 23:52:56 +0200

    Added styling for tablet display

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 100);
 });
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closedBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closedBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+closedBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  closedBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+});
