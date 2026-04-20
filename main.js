9ea14c96aded3f9f7448cdd4095c9257f2937e8d
NikolaTomic00 <tomicnikolaftn@gmail.com>
2026-04-20 17:10:25 +0200

    added swiper.js on testimonial section

--- main.js
+++ main.js
@@ -20,3 +20,31 @@ closedBtn.addEventListener("click", () => {
   closedBtn.style.display = "none";
   menuBtn.style.display = "inline-block";
 });
+
+const swiper = new Swiper(".testimonial__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
